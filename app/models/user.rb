c0caa0a731858dfa08b436d2c86c18edab83d942
Tony Allevato <allevato@vt.edu>
2012-07-09 09:34:41 -0400

    Removed registerable and recoverable Devise attributes from User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :ldap_authenticatable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation,
