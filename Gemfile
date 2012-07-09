e157e896d37bbdaec87310a5989c4791dc1f9d0d
Tony Allevato <allevato@vt.edu>
2012-07-08 22:17:05 -0400

    Added Devise dependency and User model. Created placeholder home controller as root route.

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'rails', '3.2.6'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
 gem 'json'
 
 # Gems used only for assets and not required
@@ -22,6 +21,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'devise'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pythy::Application.routes.draw do
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Pythy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
