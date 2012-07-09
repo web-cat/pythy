5c5db8b8e9768e4febe473b1091d4d1328c9d7be
Tony Allevato <allevato@vt.edu>
2012-07-08 21:39:34 -0400

    Created new Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pythy::Application.config.secret_token = 'a0e3c651211d9a2e88b4e51cb20bd2002f75c7e2234d41b44ff1bc8c5382d5958fdaa28b0ecfc622941cc9f3a037904c992bb173637ae08a493019854405be9e'
