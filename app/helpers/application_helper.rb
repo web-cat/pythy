8ada50af88e303b5570f780ee9886be0c4e4cd75
Tony Allevato <allevato@vt.edu>
2012-08-17 20:53:59 -0400

    Added Devise helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,41 @@ module ApplicationHelper
     User.all.empty?
   end
 
+
+  # Devise helpers
+
+  # -------------------------------------------------------------
+  def resource_name
+    :user
+  end
+
+  # -------------------------------------------------------------
+  def resource
+    @resource ||= User.new
+  end
+
+  # -------------------------------------------------------------
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  # -------------------------------------------------------------
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource_name)
+
+    html = <<-HTML
+<div id="error_explanation">
+<h2>#{sentence}</h2>
+<ul>#{messages}</ul>
+</div>
+HTML
+
+    html.html_safe
+  end
+
 end
