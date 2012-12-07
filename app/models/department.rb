1fb0b7a0a85c0fc888b8d0454ebf2080b08d42a9
Tony Allevato <allevato@vt.edu>
2012-12-07 08:02:45 -0500

    Added validations for presence of name and abbreviation.

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,6 +3,9 @@ class Department < ActiveRecord::Base
   belongs_to  :institution
   has_many    :courses
 
+  validates :name, presence: true
+  validates :abbreviation, presence: true
+
   attr_accessible :abbreviation, :institution_id, :name
 
 end
