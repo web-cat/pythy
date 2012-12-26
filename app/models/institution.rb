007c0c4f51b83b28970cf16ce9ba6c70f934fa62
Tony Allevato <allevato@vt.edu>
2012-12-26 09:42:17 -0500

    Added uniqueness validations to institutions.

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -3,6 +3,9 @@ class Institution < ActiveRecord::Base
   has_many    :departments
   has_many    :users
 
-  attr_accessible :display_name, :domain
+  attr_accessible :display_name, :domain, :abbreviation
+
+  validates :abbreviation, uniqueness: { case_sensitive: false }
+  validates :domain, uniqueness: { case_sensitive: false }
 
 end
