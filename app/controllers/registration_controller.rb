0c2df8267e275849f24a02255385f8d4decbe2fd
Tony Allevato <allevato@vt.edu>
2012-12-26 12:46:21 -0500

    Added logging for registration controller events.

--- app/controllers/registration_controller.rb
+++
@@ -1,2 +0,0 @@
-class RegistratrionsController < Devise:RegistrationCrontroller
-end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,15 @@
 # temporary file to handle user registrions
 class RegistrationsController < Devise::RegistrationsController
+
+  after_filter :log_event, only: [:create, :destroy]
+
+  private
+
+  # -------------------------------------------------------------
+  def log_event
+    action = "registrations/#{action_name}"
+    log = ActivityLog.new(user: current_user, action: action, info: nil)
+    log.save
+  end
+
 end
