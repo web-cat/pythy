bb3476336a9b4417f46fbfa0bb6acceefb7ad6ed
Tony Allevato <allevato@vt.edu>
2012-12-26 12:45:47 -0500

    Style cleanup.

--- app/models/global_role.rb
+++ app/models/global_role.rb
@@ -1,4 +1,5 @@
 class GlobalRole < ActiveRecord::Base
+
   attr_accessible :can_edit_system_configuration, 
                   :can_manage_all_courses,
                   :can_create_courses,
@@ -15,9 +16,11 @@ class GlobalRole < ActiveRecord::Base
   STUDENT_ID = 2
   INSTRUCTOR_ID = 3
 
+
+  # -------------------------------------------------------------
   def check_builtin?
     errors.add :base, "Cannot delete built-in roles." if builtin?
-    
     errors.blank?
   end
+
 end
