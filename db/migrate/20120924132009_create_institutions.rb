b362e947f520495719ce278448fb06ca495f25c9
Tony Allevato <allevato@vt.edu>
2012-12-26 09:33:40 -0500

    Updated CreateInstitutions migration.

--- db/migrate/20120924132009_create_institutions.rb
+++ db/migrate/20120924132009_create_institutions.rb
@@ -1,10 +1,16 @@
 class CreateInstitutions < ActiveRecord::Migration
+
   def change
     create_table :institutions do |t|
-      t.string :domain
+      t.string :abbreviation
       t.string :display_name
+      t.string :domain
 
       t.timestamps
     end
+
+    add_index :institutions, :abbreviation, unique: true
+    add_index :institutions, :domain,       unique: true
   end
+
 end
