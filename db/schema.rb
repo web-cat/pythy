f13ccc789e25bda08279d9cb8671b850d3816bdf
Tony Allevato <allevato@vt.edu>
2012-12-26 15:56:33 -0500

    Random schema change.

--- db/schema.rb
+++ db/schema.rb
@@ -155,7 +155,7 @@ ActiveRecord::Schema.define(:version => 20121224211905) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
