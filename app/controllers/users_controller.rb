c602a44962dfb674122d143aedf440dccda3a0a4
Tony Allevato <allevato@vt.edu>
2012-12-26 12:45:39 -0500

    Comment cleanup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  # -------------------------------------------------------------
   load_and_authorize_resource
 
+  # -------------------------------------------------------------
   # GET /users
   # GET /users.json
   def index
