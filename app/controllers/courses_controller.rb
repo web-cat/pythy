ff2b129d9a538eb33d097b887bffc83de7c41ff3
Tony Allevato <allevato@vt.edu>
2012-12-26 09:45:14 -0500

    Migrated to Ruby 1.9 hash syntax.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,83 +1,108 @@
 class CoursesController < ApplicationController
+
+  # -------------------------------------------------------------
   # GET /courses
   # GET /courses.json
   def index
+    @department = Department.find(params[:department_id])
+
     @courses = Course.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => @courses }
+      format.json { render json: @courses }
     end
   end
 
+
+  # -------------------------------------------------------------
   # GET /courses/1
   # GET /courses/1.json
   def show
     @course = Course.find(params[:id])
+    @department = @course.department
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render :json => @course }
+      format.json { render json: @course }
     end
   end
 
+
+  # -------------------------------------------------------------
   # GET /courses/new
   # GET /courses/new.json
   def new
-    @course = Course.new
+    @department = Department.find(params[:department_id])
+    
+    @course = @department ?
+      @department.courses.build : Course.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render :json => @course }
+      format.json { render json: @course }
     end
   end
 
+
+  # -------------------------------------------------------------
   # GET /courses/1/edit
   def edit
     @course = Course.find(params[:id])
+    @department = @course.department
   end
 
+
+  # -------------------------------------------------------------
   # POST /courses
   # POST /courses.json
   def create
     @course = Course.new(params[:course])
+    @department = @course.department
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, :notice => 'Course was successfully created.' }
-        format.json { render :json => @course, :status => :created, :location => @course }
+        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.json { render json: @course, status: :created, location: @course }
       else
-        format.html { render :action => "new" }
-        format.json { render :json => @course.errors, :status => :unprocessable_entity }
+        format.html { render action: "new" }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
+
+  # -------------------------------------------------------------
   # PUT /courses/1
   # PUT /courses/1.json
   def update
     @course = Course.find(params[:id])
+    @department = @course.department
 
     respond_to do |format|
       if @course.update_attributes(params[:course])
-        format.html { redirect_to @course, :notice => 'Course was successfully updated.' }
+        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render :action => "edit" }
-        format.json { render :json => @course.errors, :status => :unprocessable_entity }
+        format.html { render action: "edit" }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
+
+  # -------------------------------------------------------------
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
     @course = Course.find(params[:id])
+    @department = @course.department
     @course.destroy
 
     respond_to do |format|
-      format.html { redirect_to courses_url }
+      format.html { redirect_to department_courses_url(@department) }
       format.json { head :no_content }
     end
   end
+
 end
