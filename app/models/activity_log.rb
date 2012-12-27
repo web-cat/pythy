242d5d608d5ece548294ca5a6765f02f87386ae8
Tony Allevato <allevato@vt.edu>
2012-12-27 12:01:45 -0500

    Added documentation.

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -1,3 +1,7 @@
+# =============================================================================
+# Public: Represents logged activity by a user in the system. Activities that
+# are logged include session creation/destruction (i.e., sign-in and sign-out),
+# 
 class ActivityLog < ActiveRecord::Base
 
   belongs_to :user
@@ -9,7 +13,17 @@ class ActivityLog < ActiveRecord::Base
   default_scope includes(:user).order('activity_logs.created_at desc')
 
 
+  #~ Instance methods .........................................................
+
   # -------------------------------------------------------------
+  # Public: Gets all of the actions currently in the activity_log table.
+  # Mainly used for populating typeahead fields in filter forms.
+  #
+  # prefix - the prefix to filter action names by
+  #
+  # Returns an Array containing all of the action names that start with
+  # the prefix, sorted in ascending order.
+  #
   def self.all_actions(prefix = '')
     self.uniq.where(self.arel_table[:action].matches(
       "#{prefix}%")).reorder('action asc').pluck(:action)
