5ff238f94b05c56777bbd4429f0d7a1ef986544c
Tony Allevato <allevato@vt.edu>
2012-12-27 12:01:30 -0500

    Added full_name_or_none helper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # -------------------------------------------------------------
+  def full_name_or_none(user, options={})
+    if !user.full_name.blank?
+      user.full_name
+    elsif options[:muted]
+      raw(content_tag :span, '(none given)', class: 'muted')
+    else
+      '(none given)'
+    end
+  end
+
 end
