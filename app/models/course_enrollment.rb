a278ea73eb0c9e48417d8c5b90c2fe887fac67bd
Tony Allevato <allevato@vt.edu>
2012-12-29 11:03:53 -0500

    Added some helper methods to get student/non-student enrollments.

--- app/models/course_enrollment.rb
+++ app/models/course_enrollment.rb
@@ -3,5 +3,26 @@ class CourseEnrollment < ActiveRecord::Base
   belongs_to :course_offering
   belongs_to :course_role
   
-  attr_accessible :user, :course_offering, :course_role, :user_id, :course_offering_id, :course_role_id
+  attr_accessible :user, :course_offering, :course_role, :user_id,
+    :course_offering_id, :course_role_id
+
+
+  SQL_HAS_ANY_PERMISSIONS = <<-SQL
+    `course_roles`.`can_manage_course`          = 1 or
+    `course_roles`.`can_manage_assignments`     = 1 or
+    `course_roles`.`can_grade_submissions`      = 1 or
+    `course_roles`.`can_view_other_submissions` = 1
+  SQL
+
+  SQL_HAS_NO_PERMISSIONS = <<-SQL
+    `course_roles`.`can_manage_course`          = 0 and
+    `course_roles`.`can_manage_assignments`     = 0 and
+    `course_roles`.`can_grade_submissions`      = 0 and
+    `course_roles`.`can_view_other_submissions` = 0
+  SQL
+
+
+  scope :staff, joins(:course_role).where(SQL_HAS_ANY_PERMISSIONS)
+  scope :students, joins(:course_role).where(SQL_HAS_NO_PERMISSIONS)
+
 end
--- app/models/course_offering.rb
+++ app/models/course_offering.rb
@@ -1,13 +1,78 @@
 class CourseOffering < ActiveRecord::Base
 
   belongs_to  :course
+
   belongs_to  :term
 
   has_many    :course_enrollments
+
+  has_many    :users, through: :course_enrollments do
+
+    def staff
+      joins('INNER JOIN `course_roles` ON `course_enrollments`.`course_role_id` = `course_roles`.`id`').
+      where(CourseEnrollment::SQL_HAS_ANY_PERMISSIONS)
+    end
+
+    def students
+      joins('INNER JOIN `course_roles` ON `course_enrollments`.`course_role_id` = `course_roles`.`id`').
+      where(CourseEnrollment::SQL_HAS_NO_PERMISSIONS)
+    end
+
+  end
 
   attr_accessible :course_id, :crn, :label, :term_id, :url
 
   validates :term_id, presence: true
   validates :crn, presence: true
+
+
+  # -------------------------------------------------------------
+  # Public: Gets the CourseEnrollments for all users associated with the
+  # course offering who have a role that qualifies them as "staff" (that
+  # is, any user who has at least one elevated permission).
+  #
+  # Returns a relation representing CourseEnrollments for all staff users
+  # in the course offering.
+  #
+  def staff_enrollments
+    course_enrollments.staff
+  end
+
+
+  # -------------------------------------------------------------
+  # Public: Gets all staff Users in the course offering. This is a shortcut
+  # (and faster, SQL-wise) for retrieving the `staff_enrollments` relation
+  # and then retrieving the User from each of those.
+  #
+  # Returns a relation representing all staff Users in the course offering.
+  #
+  def staff
+    users.staff
+  end
+
+
+  # -------------------------------------------------------------
+  # Public: Gets the CourseEnrollments for all users associated with the
+  # course offering who have a role that qualifies them as "student" (that
+  # is, any user who has no elevated permissions).
+  #
+  # Returns a relation representing CourseEnrollments for all student
+  # users in the course offering.
+  #
+  def student_enrollments
+    course_enrollments.students
+  end
+
+
+  # -------------------------------------------------------------
+  # Public: Gets all student Users in the course offering. This is a shortcut
+  # (and faster, SQL-wise) for retrieving the `student_enrollments` relation
+  # and then retrieving the User from each of those.
+  #
+  # Returns a relation representing all student Users in the course offering.
+  #
+  def students
+    users.students
+  end
 
 end
