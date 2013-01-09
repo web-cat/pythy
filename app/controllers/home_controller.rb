f2028f2b9bc64e6347d73e065315a66583590f22
Tony Allevato <allevato@vt.edu>
2013-01-09 09:37:44 -0500

    Added sorting for elements on the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < FriendlyUrlController
     if @offerings.any?
       @not_started_assignments = []
       @started_assignments = []
+      @examples = []
 
       @offerings.each do |offering|
         assignments = offering.assignment_offerings.visible
@@ -23,7 +24,13 @@ class HomeController < FriendlyUrlController
             @not_started_assignments |= [assignment]
           end
         end
+
+        @examples |= offering.example_repositories.all
       end
+
+      @examples.sort! { |a, b| b.created_at <=> a.created_at }
+      @started_assignments.sort! { |a, b| a.updated_at <=> b.updated_at }
+      @not_started_assignments.sort! { |a, b| b.effectively_due_at <=> a.effectively_due_at }
 
       respond_to do |format|
         format.html do
