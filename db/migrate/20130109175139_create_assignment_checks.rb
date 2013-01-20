de6333c725e69bf64b43b1eff66881ed013549ad
Tony Allevato <allevato@vt.edu>
2013-01-19 20:57:07 -0500

    Fixed incorrect column name in migration.

--- db/migrate/20130109175139_create_assignment_checks.rb
+++ db/migrate/20130109175139_create_assignment_checks.rb
@@ -3,7 +3,7 @@ class CreateAssignmentChecks < ActiveRecord::Migration
   def change
     create_table :assignment_checks do |t|
       t.integer :assignment_repository_id
-      t.integer :index
+      t.integer :number
       t.integer :status
       t.decimal :overall_score, precision: 16, scale: 8
       t.text :extra
