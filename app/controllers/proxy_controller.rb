915cf9c15cf5578b3f59dc47aed9e9a7c34099b9
Tony Allevato <allevato@vt.edu>
2013-02-19 21:37:05 -0500

    Proxy controller can now handle 301 redirects.

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -9,11 +9,25 @@ class ProxyController < ApplicationController
 
     # TODO verify hash
 
-    result = Net::HTTP.get_response(url)
+    result = get_response_with_redirect(url)
     send_data result.body,
       type: result.content_type || 'text/plain',
       disposition: 'inline',
       status: result.code
+  end
+
+
+  private
+
+  # -------------------------------------------------------------
+  def get_response_with_redirect(uri)
+    r = Net::HTTP.get_response(uri)
+    
+    if r.code == "301"
+      r = Net::HTTP.get_response(URI.parse(r.header['location']))
+    end
+    
+    r
   end
 
 end
