cbc865f233c4d8edd189de115acd766256d46a34
Tony Allevato <allevato@vt.edu>
2013-03-14 19:29:39 -0400

    Very minor improvements over the current horrible urlopen sandbox hack.

--- python/pythy/sandbox.py
+++ python/pythy/sandbox.py
@@ -31,7 +31,7 @@ class SandboxedModules:
     module = imp.load_module('request', *module_info)
 
     if fullname == 'urllib.request':
-      module.urlopen = safe_urlopen
+      module.urlopen = call_urlopen
 
     sys.modules[fullname] = module
     return module
@@ -45,7 +45,18 @@ _fixed_currencies = {
 }
 
 # -------------------------------------------------------------
-def safe_urlopen(url):
+def call_urlopen(url):
+  return _sandboxed_urlopen(url)
+
+
+# -------------------------------------------------------------
+def set_urlopen(fn):
+  global _sandboxed_urlopen
+  _sandboxed_urlopen = fn
+
+
+# -------------------------------------------------------------
+def _safe_urlopen(url):
   m = re.match(r"http://www.google.com/ig/calculator\?(?:hl=en&)?q=([0-9.]+)USD=\?(.*)", url)
   if m:
     groups = m.groups()
@@ -56,14 +67,20 @@ def safe_urlopen(url):
       return io.StringIO(_fixed_currencies[key])
     else:
       return io.StringIO('{lhs: "100 U.S. dollars",rhs: "75.7002271 Euros",error: "",icc: true}')
+  elif re.match(r"http://people.cs.vt.edu/allevato/cs2984-python/project2/words.txt", url):
+    return io.StringIO('sampleword\n')
   else:
     raise AssertionError('The URL you requested was formatted incorrectly or not allowed.')
+
+
+def sandboxModule(name, dict):
+  _sandbox.add_module(name, dict)
 
 
 _sandbox = SandboxedModules()
 sys.meta_path.insert(0, _sandbox)
 
 urllib_request = {}
-urllib_request['urlopen'] = safe_urlopen
+urllib_request['urlopen'] = _safe_urlopen
 
 _sandbox.add_module('urllib.request', urllib_request)
