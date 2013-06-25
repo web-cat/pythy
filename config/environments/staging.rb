aba5f410b663eb1d73255c9bbfc1f9e07d83a1e6
Tony Allevato <allevato@vt.edu>
2013-06-25 19:32:50 -0400

    Updated mailer host in config.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,6 +20,8 @@ Pythy::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { host: 'pythy.cs.vt.edu' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
