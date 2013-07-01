0c82de5379893820a5b3b42d5ce3a1425182bc96
Tony Allevato <allevato@vt.edu>
2013-07-01 12:46:08 -0400

    Fixed the deploy script to properly link media folders after deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -124,3 +124,50 @@ end
 
 before 'deploy:setup',       'db:configure'
 after  'deploy:update_code', 'db:symlink'
+
+
+# ==============================
+# Uploads
+# ==============================
+
+namespace :uploads do
+
+  desc <<-EOD
+    Creates the upload folders unless they exist
+    and sets the proper upload permissions.
+  EOD
+  task :setup, except: { no_release: true } do
+    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+    sudo "mkdir -p #{dirs.join(' ')}"
+    sudo "chmod g+w #{dirs.join(' ')}"
+
+    dirs = protected_dirs.map { |d| File.join(shared_path, d) }
+    sudo "mkdir -p #{dirs.join(' ')}"
+    sudo "chmod g+w #{dirs.join(' ')}"
+  end
+
+  desc <<-EOD
+    [internal] Creates the symlink to uploads shared folder
+    for the most recently deployed version.
+  EOD
+  task :symlink, except: { no_release: true } do
+    #run "rm -rf #{release_path}/public/uploads"
+    run "ln -nfs #{shared_path}/uploads #{latest_release}/public/uploads"
+
+    #run "rm -rf #{release_path}/public/system"
+    run "ln -nfs #{shared_path}/m #{latest_release}/public/m"
+  end
+
+  desc <<-EOD
+    [internal] Computes uploads directory paths
+    and registers them in Capistrano environment.
+  EOD
+  task :register_dirs do
+    set :uploads_dirs,    %w(m uploads)
+    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+  end
+
+  after       "deploy:finalize_update", "uploads:symlink"
+  on :start,  "uploads:register_dirs"
+
+end
