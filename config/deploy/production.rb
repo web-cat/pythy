6e023309bef0a828ef9e9259148e290bd79847c3
s-edwards <edwards@cs.vt.edu>
2013-09-11 08:10:18 -0400

    Updated skulpt to 3-ish branch.  Removed turbolinks.  Added Eclipse/aptana project file. Minor cap deployment tweaks.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,17 @@
+default_run_options[:pty] = true
+
 set :user,        'pythy'
 set :db_user,     'pythy'
 
 set :branch,      'stable'
 set :rails_env,   'production'
 set :deploy_to,   "/home/#{user}/rails"
+
+after 'uploads:symlink', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,17 @@
+default_run_options[:pty] = true
+
 set :user,        'pythystaging'
 set :db_user,     'pythystaging'
 
 set :branch,      'master'
 set :rails_env,   'staging'
 set :deploy_to,   "/home/#{user}/rails"
+
+after 'uploads:symlink', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
