4f396a2dfabdea26a8377d2e4fa6de023582a8c9
ArturAguiar <arturaguiar@hotmail.com>
2013-09-23 16:20:13 -0400

    Initial setup page now redirects to System Configuration page. Warning added to dropdown in case the System Configuration is not created. Fixed upload roster default role.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,38 @@ class RegistrationsController < Devise::RegistrationsController
 
   after_filter :log_event, only: [:create, :destroy]
 
+  # Override devise's default create action to redirect administrator
+  # to the System Configuration page.
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        if SystemConfiguration.count == 0 && @user.global_role_id == 1
+          flash[:notice] += " Please setup the system configuration paths now."
+          respond_with resource, :location => edit_system_configuration_path
+        else
+          respond_with resource, :location => after_sign_up_path_for(resource)
+        end        
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        if SystemConfiguration.count == 0 && @user.global_role_id == 1
+          flash[:notice] += " Please setup the system configuration paths now."
+          respond_with resource, :location => edit_system_configuration_path
+        else
+          respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        end        
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   private
 
   # -------------------------------------------------------------
